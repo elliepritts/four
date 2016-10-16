290d02fe89f78c5875b991b91b6a4b47833b2922
Ellie Pritts <ellie@elliepritts.com>
2016-10-16 18:48:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# FOUR
+
+1. Start Screen
+On start, four squares appear in the center of the screen. Upon tapping, the screen will zoom into the experience.
+Once in the element, you cannot leave unless you refresh the page.
+
+2. Element Screen
+This screen has a unique background and four tappable rectangles.
+
+Upon tapping a rectangle, an audio loop starts and the rectangle has a glow around its edges.
+Tapping again turns off the audio and removes the glow.
---
+++ src/assets/scripts.js
@@ -0,0 +1,39 @@
+$(function() {
+	$('.start-screen').on('click', '.element', function() {
+		var element = $(this).attr('data-element');
+
+		$('.start-screen').hide();
+		$('.element-screen[data-element="' + element +'"]')
+			.addClass('is-visible');
+		$('body').addClass(element);
+	});
+
+	$('button').click(function() {
+		var audio = $(this).find('audio').get(0);
+		var shouldPlay = audio.paused;
+
+		pauseAllAudio();
+
+		if (shouldPlay) {
+			$(this)
+				.addClass('is-playing')
+				.removeClass('is-no-longer-playing');
+
+			audio.currentTime = 0;
+			audio.volume = .05; // debug
+			
+			setTimeout(function() {
+				audio.play();
+			}, 25);
+		}
+	});
+
+	function pauseAllAudio() {
+		$('.is-playing')
+			.removeClass('is-playing')
+			.addClass('is-no-longer-playing')
+			.find('audio').each(function() {
+				$(this).get(0).pause();
+			});
+	}
+});
